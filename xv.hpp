ee04e16907c6e5c724f2f4b39b0cfee612ebceb4
emlin <49819627+emlinhax@users.noreply.github.com>
2023-11-22 10:18:54 +0100

    support more types and beautified

--- xv.hpp
+++ xv.hpp
@@ -1,138 +1,129 @@
 #pragma once
-#include <stdlib.h>
-
-typedef long long i64;
+#pragma warning(disable: 4244) // T to i64 loss of data
 
 #define MASK1 0xAAAAAAA
 #define MASK2 0x5555555
 
+typedef __int64 i64;
+
 enum e_cypher_mode
 {
-    XOR0 = 0,
-    XOR1 = 1,
-    XOR2 = 2,
-    XOR3 = 3,
+    XOR1,
+    XOR2,
+    XOR3,
 };
 
 template<typename T>
 class xval;
 
-template<typename T>
 class common
 {
 public:
-    static __forceinline T rotate_left(T value, int count) {
+    static __forceinline i64 rotate_left(i64 value, i64 count) {
         const unsigned int mask = (CHAR_BIT * sizeof(value) - 1);
         count &= mask;
         return (value << count) | (value >> (-count & mask));
     }
 
-    static __forceinline int rand_int(int min, int max) {
+    static __forceinline i64 rand_int(i64 min, i64 max) {
         return min + rand() % (max - min + 1);
     }
 };
 
-template<typename T>
-class __xv_e_0
-{
-public:
-    static __forceinline T encrypt(T data, T key) {
-        return data ^ key;
-    }
 
-    static __forceinline T decrypt(T data, T key) {
-        return data ^ key;
-    }
-};
-
-template<typename T>
 class __xv_e_1
 {
 public:
-    static __forceinline T encrypt(T data, T key) {
-        T result = data ^ key;
-        result = common<T>::rotate_left(result, 5);
+    static __forceinline i64 encrypt(i64 data, i64 key) {
+        i64 result = data ^ key;
+        result = common::rotate_left(result, 5);
         result ^= (key >> 2);
         return result;
     }
 
-    static __forceinline T decrypt(T encrypted_data, T key) {
+    static __forceinline i64 decrypt(i64 encrypted_data, i64 key) {
         encrypted_data ^= (key >> 2);
-        encrypted_data = common<T>::rotate_left(encrypted_data, -5);
+        encrypted_data = common::rotate_left(encrypted_data, -5);
         return encrypted_data ^ key;
     }
 };
 
-template<typename T>
 class __xv_e_2
 {
 public:
-    static __forceinline T encrypt(T data, T key) {
-        T result = data ^ key;
+    static __forceinline i64 encrypt(i64 data, i64 key) {
+        i64 result = data ^ key;
 
-        result = common<T>::rotate_left(result, key % 7);
+        result = common::rotate_left(result, key % 7);
         result ^= (key >> 3) & (key << 2);
-        result = common<T>::rotate_left(result, 3);
-        result ^= (key & (common<T>::rotate_left(key, 2)));
+        result = common::rotate_left(result, 3);
+        result ^= (key & (common::rotate_left(key, 2)));
 
         return result;
     }
 
-    static __forceinline T decrypt(T encrypted_data, T key) {
-        encrypted_data ^= (key & (common<T>::rotate_left(key, 2)));
-        encrypted_data = common<T>::rotate_left(encrypted_data, -3);
+    static __forceinline i64 decrypt(i64 encrypted_data, i64 key) {
+        encrypted_data ^= (key & (common::rotate_left(key, 2)));
+        encrypted_data = common::rotate_left(encrypted_data, -3);
         encrypted_data ^= (key >> 3) & (key << 2);
-        encrypted_data = common<T>::rotate_left(encrypted_data, -(key % 7));
+        encrypted_data = common::rotate_left(encrypted_data, -(key % 7));
 
         return encrypted_data ^ key;
     }
 };
 
-template<typename T>
 class __xv_e_3
 {
 public:
-    static int _ok;
+    static __int64 _ok;
 
-    static __forceinline T encrypt(T data, T key) {
-        T result = data ^ key;
+    static __forceinline i64 encrypt(i64 data, i64 key) {
+        i64 result = data ^ key;
 
-        result = common<T>::rotate_left(result, key % 7);
+        result = common::rotate_left(result, key % 7);
         result ^= (key >> 3) & (key << 2);
-        result = common<T>::rotate_left(result, 3);
-        result ^= (key & (common<T>::rotate_left(key, 2)));
+        result = common::rotate_left(result, 3);
+        result ^= (key & (common::rotate_left(key, 2)));
         result ^= _ok;
 
         return result;
     }
 
-    static __forceinline T decrypt(T encrypted_data, T key) {
+    static __forceinline i64 decrypt(i64 encrypted_data, i64 key) {
         encrypted_data ^= _ok;
-        encrypted_data ^= (key & (common<T>::rotate_left(key, 2)));
-        encrypted_data = common<T>::rotate_left(encrypted_data, -3);
+        encrypted_data ^= (key & (common::rotate_left(key, 2)));
+        encrypted_data = common::rotate_left(encrypted_data, -3);
         encrypted_data ^= (key >> 3) & (key << 2);
-        encrypted_data = common<T>::rotate_left(encrypted_data, -(key % 7));
+        encrypted_data = common::rotate_left(encrypted_data, -(key % 7));
 
         return encrypted_data ^ key;
     }
 };
-template<typename T>
-int __xv_e_3<T>::_ok = (int)__TIMESTAMP__;
+__int64 __xv_e_3::_ok = reinterpret_cast<__int64>(__TIMESTAMP__);
 
 template<typename T>
 class xval
 {
     e_cypher_mode m;
-    T v;
-    T k;
+    i64 v;
+    i64 k;
 
 public:
+    __forceinline xval()
+    {
+        srand(time(NULL));
+
+        m = (e_cypher_mode)common::rand_int(0,2);
+        k = common::rand_int(0x0001000000, 0xFFFFFFFFFF);
+        set(0);
+    }
+
     __forceinline xval(T _v)
     {
         srand(time(NULL));
 
-        m = (e_cypher_mode)common<T>::rand_int(0, 2);
-        k = common<T>::rand_int(0x0001000000, 0xFFFFFFFFFF);
+        m = (e_cypher_mode)common::rand_int(0,2);
+        k = common::rand_int(0x0001000000, 0xFFFFFFFFFF);
         set(_v);
     }
 
@@ -141,7 +132,7 @@ public:
         srand(time(NULL));
 
         m = _m;
-        k = common<T>::rand_int(0x0001000000, 0xFFFFFFFFFF);
+        k = common::rand_int(0x0001000000, 0xFFFFFFFFFF);
         set(_v);
     }
 
@@ -154,14 +145,12 @@ public:
     {
         switch (m)
         {
-        case e_cypher_mode::XOR0:
-            return __xv_e_0<T>::decrypt(v, k);
         case e_cypher_mode::XOR1:
-            return __xv_e_1<T>::decrypt(v, k);
+            return __xv_e_1::decrypt(v, k);
         case e_cypher_mode::XOR2:
-            return __xv_e_2<T>::decrypt(v, k);
+            return __xv_e_2::decrypt(v, k);
         case e_cypher_mode::XOR3:
-            return __xv_e_3<T>::decrypt(v, k);
+            return __xv_e_3::decrypt(v, k);
         }
 
         return 0;
@@ -171,17 +160,14 @@ public:
     {
         switch (m)
         {
-        case e_cypher_mode::XOR0:
-            v = __xv_e_0<T>::encrypt(_v, k);
-            break;
         case e_cypher_mode::XOR1:
-            v = __xv_e_1<T>::encrypt(_v, k);
+            v = __xv_e_1::encrypt(_v, k);
             break;
         case e_cypher_mode::XOR2:
-            v = __xv_e_2<T>::encrypt(_v, k);
+            v = __xv_e_2::encrypt(_v, k);
             break;
         case e_cypher_mode::XOR3:
-            v = __xv_e_3<T>::encrypt(__xv_e_3<T>::encrypt(_v, k), k);
+            v = __xv_e_3::encrypt(_v, k);
             break;
         }
     }
